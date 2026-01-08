f44dae05d86288f9bfd8d441d4d488589a384c01
Eden Gress <gresseden@gmail.com>
2026-01-07 19:09:46 -0500

    mergin new branch

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,25 +1,25 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
-import { getAuth, GoogleAuthProvider } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAmEA-lUyOWApCknhTRJBK77oVVYzi67J4",
-  authDomain: "vsi-catalog.firebaseapp.com",
-  projectId: "vsi-catalog",
-  storageBucket: "vsi-catalog.appspot.com",
-  messagingSenderId: "355024279103",
-  appId: "1:355024279103:web:f2905793149fb9b6e24a8a"
+  apiKey: "AIzaSyAJLh_M5LSgF7Hx62eqSdTyaKyDxS0VCdo",
+  authDomain: "gallery-app-a6274.firebaseapp.com",
+  projectId: "gallery-app-a6274",
+  storageBucket: "gallery-app-a6274.firebasestorage.app",
+  messagingSenderId: "188607953037",
+  appId: "1:188607953037:web:cb7ce6a01a182b79858ac4",
+  measurementId: "G-8FC2HERSEL",
 };
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig)
-const auth = getAuth(firebaseApp)
-const db = getFirestore(firebaseApp)
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
 const provider = new GoogleAuthProvider(); // instancia del proveedor de Google
 
-
-export { auth, db, provider }
+export { auth, db, provider };
